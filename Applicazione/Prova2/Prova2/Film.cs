b8279105975b6bc44cb10912f53a8a8ff56bbd78
Barbara Mazzoni <alessia.lombardi4@studio.unibo.it>
2021-06-16 20:41:42 +0200

    Prova Layout Applicazione

---
+++ Applicazione/Prova2/Prova2/Film.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prova2
+{
+    public partial class Film : Form
+    {
+        public Film()
+        {
+            InitializeComponent();
+        }
+
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            MainForm main = new MainForm();
+            main.Show();
+            main.BringToFront();
+            this.Hide();
+        }
+    }
+}
